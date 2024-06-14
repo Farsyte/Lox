3c8a5226eee5a2f72c0deb0b9c9263763ac2603b
Greg Limes <greg.limes@gmail.com>
2024-06-14 19:05:10 -0400

    impl Scanner class constructor and state

--- j/jlox/src/farsyte/lox/Scanner.java
+++ j/jlox/src/farsyte/lox/Scanner.java
@@ -1,16 +1,22 @@
 package farsyte.lox;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static farsyte.lox.TokenType.*;
 
 class Scanner {
-    Scanner(String s) {
-        System.out.println("STUB: Scanner("+s+")");
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    Scanner(String source) {
+        this.source = source;
     }
 
     List<Token> scanTokens() {
-        System.out.println("STUB: Scanner.scanTokens()");
+        System.out.println("STUB: Scanner.scanTokens() over \"" + source + "\"");
         return new ArrayList<Token>();
     }
-
 }
