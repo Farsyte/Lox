f8c542f5896094c42887fd26e22d5765e1894281
Greg Limes <greg.limes@gmail.com>
2024-06-14 19:11:53 -0400

    impl Scanner.scanToken()

--- j/jlox/src/farsyte/lox/Scanner.java
+++ j/jlox/src/farsyte/lox/Scanner.java
@@ -30,11 +30,32 @@ class Scanner {
     }
 
     private void scanToken() {
-        System.out.println("STUB: scanToken()");
+        char c = advance();
+        switch (c) {
+        case '(': addToken(LEFT_PAREN); break;
+        case ')': addToken(RIGHT_PAREN); break;
+        case '{': addToken(LEFT_BRACE); break;
+        case '}': addToken(RIGHT_BRACE); break;
+        case ',': addToken(COMMA); break;
+        case '.': addToken(DOT); break;
+        case '-': addToken(MINUS); break;
+        case '+': addToken(PLUS); break;
+        case ';': addToken(SEMICOLON); break;
+        case '*': addToken(STAR); break;
+        }
     }
 
     private boolean isAtEnd() {
         System.out.println("STUB: scanToken()");
         return true;
+    }
+
+    private char advance() {
+        System.out.println("STUB: advance()");
+        return '\n';
+    }
+
+    private void addToken(TokenType type) {
+        System.out.println("STUB: addToken("+type+")");
     }
 }
