600b2027f9e54a638822934ab6c2dd775dca8f92
Greg Limes <greg.limes@gmail.com>
2024-06-15 14:22:36 -0400

    impl Interpreter.visitLiteralExpr()

--- j/jlox/src/farsyte/lox/Interpreter.java
+++ j/jlox/src/farsyte/lox/Interpreter.java
@@ -4,7 +4,7 @@ class Interpreter implements Expr.Visitor<Object> {
 
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
-        throw new NotImplementedException(", unreachable at end.");
+        return expr.value;
     }
 
     @Override
