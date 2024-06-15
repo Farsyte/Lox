36267c26f959e5bc23cf5e35a092815fac29cf0d
Greg Limes <greg.limes@gmail.com>
2024-06-14 23:55:04 -0400

    improve error recovery
    - add Parser.synchronize()
    - proper error at end of primary()
    - add Parser.parse()

--- j/jlox/src/farsyte/lox/Parser.java
+++ j/jlox/src/farsyte/lox/Parser.java
@@ -14,6 +14,14 @@ class Parser {
         this.tokens = tokens;
     }
 
+    Expr parse() {
+        try {
+            return expression();
+        } catch (ParseError error) {
+            return null;
+        }
+    }
+
     private Expr expression() {
         // expression → equality ;
         return equality();
@@ -96,7 +104,7 @@ class Parser {
             consume(RIGHT_PAREN, "Expect ')' after expression.");
             return new Expr.Grouping(expr);
         }
-        throw new NotImplementedException(", falling off the end");
+        throw error(peek(), "Expect expression.");
     }
 
     private Token consume(TokenType type, String message) {
@@ -108,6 +116,26 @@ class Parser {
     private ParseError error(Token token, String message) {
         Lox.error(token, message);
         return new ParseError();
+    }
+
+    private void synchronize() {
+        advance();
+
+        while (!isAtEnd()) {
+            if (previous().type == SEMICOLON) return;
+            switch (peek().type) {
+            case CLASS:
+            case FUN:
+            case VAR:
+            case FOR:
+            case IF:
+            case WHILE:
+            case PRINT:
+            case RETURN:
+                return;
+            }
+            advance();
+        }
     }
 
     private boolean match(TokenType... types) {
