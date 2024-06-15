90cd38edeefdfb378237dbf37d860809c9105853
Greg Limes <greg.limes@gmail.com>
2024-06-15 16:15:03 -0400

    impl Environment.define(name, value)

--- j/jlox/src/farsyte/lox/Environment.java
+++ j/jlox/src/farsyte/lox/Environment.java
@@ -5,4 +5,7 @@ import java.util.Map;
 
 class Environment {
     private final Map<String, Object> values = new HashMap<>();
+    void define(String name, Object value) {
+        values.put(name, value);
+    }
 }
