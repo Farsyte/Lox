f320f98893fcf6c9451d3d880eb24d7a2db30d1d
Greg Limes <greg.limes@gmail.com>
2024-06-15 15:42:01 -0400

    impl Interpreter.visitPrintStmt

--- j/jlox/src/farsyte/lox/Interpreter.java
+++ j/jlox/src/farsyte/lox/Interpreter.java
@@ -24,7 +24,9 @@ class Interpreter implements Expr.Visitor<Object> ,
 
     @Override
     public Void visitPrintStmt(Stmt.Print stmt) {
-        throw new NotImplementedException();
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
     }
 
     @Override
