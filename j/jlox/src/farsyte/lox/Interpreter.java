980bd3641cd817b99d80758a5bfa038cb5841027
Greg Limes <greg.limes@gmail.com>
2024-06-16 12:48:47 -0400

    impl Interpreter.visitBlockStmt
    add Interpreter.executeBlock
    add build rules for tests that run a Lox program
    add prog20 test demonstrating variable scopes
    
    NOTE: the REPL parses each line in isolation,
    so blocks can't span lines. I am looking forward
    to seeing how this is resolved, or if it is not.

--- j/jlox/src/farsyte/lox/Interpreter.java
+++ j/jlox/src/farsyte/lox/Interpreter.java
@@ -33,9 +33,24 @@ class Interpreter implements Expr.Visitor<Object> ,
         stmt.accept(this);
     }
 
+    void executeBlock(List<Stmt> statements,
+                      Environment environment) {
+        Environment previous = this.environment;
+        try {
+            this.environment = environment;
+
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
+        } finally {
+            this.environment = previous;
+        }
+    }
+
     @Override
-    public Void visitBlockStmt(Stmt.Block block) {
-        throw new NotImplementedException();
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        executeBlock(stmt.statements, new Environment(environment));
+        return null;
     }
 
     @Override
