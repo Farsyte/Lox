25ce25f9a85b316be285743938d0a5fb1c201398
Greg Limes <greg.limes@gmail.com>
2024-06-16 17:36:30 -0400

    Initial Lox.main
    - class Lox with a main function
    - class NotImplementedException with helpful output
    
    May not mention these things in future commits
    - add stub functions that just throw NotImplementedException as needed
    - commit test outputs that are not just "STUB: something"
    The latter will help us see the net change in test results
    when an intermediate change would hide prior content with a STUB line.
    
    STUB is not MOCK.

---
+++ j/jlox/src/farsyte/lox/Lox.java
@@ -0,0 +1,36 @@
+package farsyte.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Lox {
+    public static void main(String[] args) throws IOException {
+        try {
+            if (args.length > 1) {
+                System.out.println("Usage: jlox [script]");
+                System.exit(64);
+            } else if (args.length == 1) {
+                runFile(args[0]);
+            } else {
+                runPrompt();
+            }
+
+        } catch (NotImplementedException e) {
+            System.err.println(e.toString());
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        throw new NotImplementedException();
+    }
+
+    private static void runPrompt() throws IOException {
+        throw new NotImplementedException();
+    }
+}
