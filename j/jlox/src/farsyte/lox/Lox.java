ee22d881d7f3102c042482aa6837f32182f1029e
Greg Limes <greg.limes@gmail.com>
2024-06-17 03:53:37 -0400

    connect parser to Lox.run
    Delete the "pure scanner" tests as now we are
    demanding expressions. Add repl10.in with some
    expressions for us to parse.

--- j/jlox/src/farsyte/lox/Lox.java
+++ j/jlox/src/farsyte/lox/Lox.java
@@ -51,10 +51,14 @@ public class Lox {
     private static void run(String source) {
 	Scanner scanner = new Scanner(source);
 	List<Token> tokens = scanner.scanTokens();
+	Parser parser = new Parser(tokens);
+	Expr expression = parser.parse();
 
-	for (Token token : tokens) {
-	    System.out.println(token);
-	}
+	// Stop if there was a syntax error.
+	if (hadError) return;
+
+	System.out.println("AST: " + new AstPrinter().print(expression));
+	System.out.println("RPN: " + new AstRpnPrinter().print(expression));
     }
 
     static void error(Token token, String message) {
