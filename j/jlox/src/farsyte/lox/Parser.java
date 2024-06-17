ac64d1a7f8beb7fd8577199176d22f0898dd08f2
Greg Limes <greg.limes@gmail.com>
2024-06-17 15:09:51 -0400

    add Parser.ifStatement
    called by Parser.statement()

--- j/jlox/src/farsyte/lox/Parser.java
+++ j/jlox/src/farsyte/lox/Parser.java
@@ -48,10 +48,26 @@ class Parser {
     }
 
     private Stmt statement() {
-	// statement → exprStmt | printStmt | block;
+	// statement → exprStmt | printStmt | ifStmt | block;
+	if (match(IF)) return ifStatement();
 	if (match(PRINT)) return printStatement();
 	if (match(LEFT_BRACE)) return new Stmt.Block(block());
 	return expressionStatement();
+    }
+
+    private Stmt ifStatement() {
+	// ifStmt → "if" "(" expression ")" statement ( "else" statement )? ;
+	consume(LEFT_PAREN, "Expect '(' after 'íf'.");
+	Expr condition = expression();
+	consume(RIGHT_PAREN, "Expect ')' after if condition.");
+
+	Stmt thenBranch = statement();
+	Stmt elseBranch = null;
+	if (match(ELSE)) {
+	    elseBranch = statement();
+	}
+
+	return new Stmt.If(condition, thenBranch, elseBranch);
     }
 
     private List<Stmt> block() {
