e20bd1d3a4167c5cf524431d37c65198e9d0f024
Greg Limes <greg.limes@gmail.com>
2024-06-17 10:16:42 -0400

    impl Interpreters.CheckNumberOperands
    use it on operands of < <= > >= - / * in visitBinaryExpr

--- j/jlox/src/farsyte/lox/Interpeter.java
+++ j/jlox/src/farsyte/lox/Interpeter.java
@@ -21,19 +21,26 @@ class Interpreter implements Expr.Visitor<Object> {
 	    break;
 
 	case MINUS:
+	    checkNumberOperands(expr.operator, left, right);
 	    return (double)left - (double)right;
 	case SLASH:
+	    checkNumberOperands(expr.operator, left, right);
 	    return (double)left / (double)right;
 	case STAR:
+	    checkNumberOperands(expr.operator, left, right);
 	    return (double)left * (double)right;
 
 	case GREATER:
+	    checkNumberOperands(expr.operator, left, right);
 	    return (double)left > (double)right;
 	case GREATER_EQUAL:
+	    checkNumberOperands(expr.operator, left, right);
 	    return (double)left >= (double)right;
 	case LESS:
+	    checkNumberOperands(expr.operator, left, right);
 	    return (double)left < (double)right;
 	case LESS_EQUAL:
+	    checkNumberOperands(expr.operator, left, right);
 	    return (double)left <= (double)right;
 
 	case BANG_EQUAL:
@@ -80,6 +87,11 @@ class Interpreter implements Expr.Visitor<Object> {
     private void checkNumberOperand(Token operator, Object operand) {
 	if (operand instanceof Double) return;
 	throw new RuntimeError(operator, "Operand must be a Number.");
+    }
+
+    private void checkNumberOperands(Token operator, Object left, Object right) {
+	if (left instanceof Double && right instanceof Double) return;
+	throw new RuntimeError(operator, "Operands must be Numbers.");
     }
 
     private boolean isTruthy(Object object) {
