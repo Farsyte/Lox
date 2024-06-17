9aef53458b88a4b6e470fa733560bc62b8d39bdb
Greg Limes <greg.limes@gmail.com>
2024-06-17 03:13:11 -0400

    add Parser with state and constructor

---
+++ j/jlox/src/farsyte/lox/Parser.java
@@ -0,0 +1,14 @@
+package farsyte.lox;
+
+import java.util.List;
+
+import static farsyte.lox.TokenType.*;
+
+class Parser {
+    private final List<Token> tokens;
+    private int current;
+
+    Parser(List<Token> tokens) {
+	this.tokens = tokens;
+    }
+}
