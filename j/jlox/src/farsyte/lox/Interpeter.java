65dcad9e27ec491527538dafc7efb1fc69fd6a0c
Greg Limes <greg.limes@gmail.com>
2024-06-17 12:44:02 -0400

    impl Interpreter.visitVarStmt()

--- j/jlox/src/farsyte/lox/Interpeter.java
+++ j/jlox/src/farsyte/lox/Interpeter.java
@@ -3,6 +3,7 @@ package farsyte.lox;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+    private Environment environment = new Environment();
 
     // No harm in keeping around the ability to
     // interpret just an expression. Might come
@@ -48,8 +49,15 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
-    public Void visitVarStmt(Stmt.Var expr) {
-	throw new NotImplementedException();
+    public Void visitVarStmt(Stmt.Var stmt) {
+	Object value = null;
+
+	if (stmt.initializer != null) {
+	    value = evaluate(stmt.initializer);
+	}
+
+	environment.define(stmt.name.lexeme, value);
+	return null;
     }
 
     @Override
