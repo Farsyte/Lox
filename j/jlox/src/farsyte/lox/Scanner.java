eae9c8d40865919fb5d75aa3cf7b8e1119e1caf6
Greg Limes <greg.limes@gmail.com>
2024-06-17 01:26:47 -0400

    impl Scanner.scanTokens

--- j/jlox/src/farsyte/lox/Scanner.java
+++ j/jlox/src/farsyte/lox/Scanner.java
@@ -10,12 +10,30 @@ import static farsyte.lox.TokenType.*;
 class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
 
     Scanner(String source) {
 	this.source = source;
     }
 
     List<Token> scanTokens() {
+	while (!isAtEnd()) {
+	    // We are at the beginning of the next lexeme.
+	    start = current;
+	    scanToken();
+	}
+
+	tokens.add(new Token(EOF, "", null, line));
+	return tokens;
+    }
+
+    private void scanToken() {
+	throw new NotImplementedException();
+    }
+
+    private boolean isAtEnd() {
 	throw new NotImplementedException();
     }
 }
