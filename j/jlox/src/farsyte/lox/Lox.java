454844bf45e7d14d6ad74e04212fdc599337cfc5
Greg Limes <greg.limes@gmail.com>
2024-06-17 03:42:08 -0400

    impl Lox.error(Token, String)

--- j/jlox/src/farsyte/lox/Lox.java
+++ j/jlox/src/farsyte/lox/Lox.java
@@ -58,7 +58,11 @@ public class Lox {
     }
 
     static void error(Token token, String message) {
-	throw new NotImplementedException();
+	if (token.type == TokenType.EOF) {
+	    report(token.line, " at end", message);
+	} else {
+	    report(token.line, " at '" + token.lexeme + "'", message);
+	}
     }
 
     static void error(int line, String message) {
