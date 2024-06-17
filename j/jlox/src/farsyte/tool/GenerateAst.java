38c8e6fa42e00046836b1c4b903b1428c69c37d7
Greg Limes <greg.limes@gmail.com>
2024-06-16 21:50:59 -0400

    write subclass in defineType
    capture Expr.java now that we have caught up with
    the hand-constructed Binary class.

--- j/jlox/src/farsyte/tool/GenerateAst.java
+++ j/jlox/src/farsyte/tool/GenerateAst.java
@@ -52,8 +52,28 @@ public class GenerateAst {
 	PrintWriter writer, String baseName,
 	String className, String fieldList) {
 
-	// SILENT STUB
+        writer.println();
+        writer.println(
+	    "    static class " + className +
+	    " extends " + baseName + " {");
 
+        // Constructor.
+        writer.println("        " + className + "(" + fieldList + ") {");
+
+        // Store parameters in fields.
+        String[] fields = fieldList.split(", ");
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("            this." + name + " = " + name + ";");
+        }
+
+        writer.println("        }");
+
+        // Fields.
+        writer.println();
+        for (String field : fields) {
+            writer.println("        final " + field + ";");
+        }
+        writer.println("    }");
     }
-
 }
