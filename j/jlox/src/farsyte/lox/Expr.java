b19fad1d8f3e034295cd928c9ed846cf3d9565b3
Greg Limes <greg.limes@gmail.com>
2024-06-16 21:37:40 -0400

    add Expr.java class
    This will soon be autogenerated, but this is what we initially
    want it to look like for the Expr.Binary node.

---
+++ j/jlox/src/farsyte/lox/Expr.java
@@ -0,0 +1,19 @@
+package farsyte.lox;
+
+import java.util.List;
+
+abstract class Expr {
+    static class Binary extends Expr {
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        final Expr left;
+        final Token operator;
+        final Expr right;
+    }
+
+    // Other expressions...
+};
