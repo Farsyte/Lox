e9e30e774520bcc482c1b15a06ed1a9e74099466
Greg Limes <greg.limes@gmail.com>
2024-06-17 01:24:01 -0400

    impl Scanner state and constructor
    - args-1 test now gets to UNIMP Scanner.scanTokens
    - repl00 test now gets to UNIMP Scanner.scanTokens

--- j/jlox/src/farsyte/lox/Scanner.java
+++ j/jlox/src/farsyte/lox/Scanner.java
@@ -1,10 +1,18 @@
 package farsyte.lox;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static farsyte.lox.TokenType.*;
 
 class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
     Scanner(String source) {
-	throw new NotImplementedException();
+	this.source = source;
     }
 
     List<Token> scanTokens() {
