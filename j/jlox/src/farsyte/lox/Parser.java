38d4db62e9432c2483bd853335a129e2dc37dec3
Greg Limes <greg.limes@gmail.com>
2024-06-17 21:14:30 -0400

    impl Parser.function()

--- j/jlox/src/farsyte/lox/Parser.java
+++ j/jlox/src/farsyte/lox/Parser.java
@@ -57,7 +57,23 @@ class Parser {
 
 	Token name = consume(IDENTIFIER, "Expect " + kind + " name.");
 
-	throw new NotImplementedException(", continue implementing");
+	consume(LEFT_PAREN, "Expect '(' after " + kind + " name.");
+	List<Token> parameters = new ArrayList<>();
+	if (!check(RIGHT_PAREN)) {
+	    do {
+		if (parameters.size() > 255) {
+		    error(peek(), "Can't have more than 255 parameters.");
+		}
+
+		parameters.add(
+		    consume(IDENTIFIER, "Expect parameter name."));
+	    } while (match(COMMA));
+	}
+	consume(RIGHT_PAREN, "Expect ')' after parameters.");
+
+	consume(LEFT_BRACE, "Expect '{' before " + kind + " body.");
+	List<Stmt> body = block();
+	return new Stmt.Function(name, parameters, body);
     }
 
     private Stmt statement() {
