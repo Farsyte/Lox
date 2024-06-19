3f81499597b85fc81d76cfc46701868aa73d0e6f
Greg Limes <greg.limes@gmail.com>
2024-06-19 17:16:07 -0400

    exercise Chunk code in main

--- c/clox/src/main.c
+++ c/clox/src/main.c
@@ -1,9 +1,15 @@
 #include "common.h"
+#include "chunk.h"
 
 int main(int argc, char **argv)
 {
     (void)argc;         /* intentionally not used */
     (void)argv;         /* intentionally not used */
+
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    freeChunk(&chunk);
 
     return 0;
 }
