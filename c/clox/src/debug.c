1a29cf940a84aa57c5fab77799a7163807a66365
Greg Limes <greg.limes@gmail.com>
2024-06-19 19:53:05 -0400

    add Binary OPs

--- c/clox/src/debug.c
+++ c/clox/src/debug.c
@@ -28,6 +28,12 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 	return constantInstruction("OP_CONSTANT", chunk, offset);
     case OP_NEGATE:
 	return simpleInstruction("OP_NEGATE", offset);
+
+    case OP_ADD: return simpleInstruction("OP_ADD", offset); break;
+    case OP_SUBTRACT: return simpleInstruction("OP_SUBTRACT", offset); break;
+    case OP_MULTIPLY: return simpleInstruction("OP_MULTIPLY", offset); break;
+    case OP_DIVIDE: return simpleInstruction("OP_DIVIDE", offset); break;
+
     case OP_RETURN:
 	return simpleInstruction("OP_RETURN", offset);
     default:
--- c/clox/src/vm.c
+++ c/clox/src/vm.c
@@ -40,6 +40,13 @@ static InterpretResult run() {
 #define READ_BYTE()	(*vm->ip++)
 #define	READ_CONSTANT()	(vm->chunk->constants->values[READ_BYTE()])
 
+#define BINARY_OP(op) do {			\
+	double b = pop();			\
+	double a = pop();			\
+	push(a op b);				\
+    } while (0)
+
+
     for (;;) {
 #ifdef	DEBUG_TRACE_EXECUTION
 	printf(" ");
@@ -65,6 +72,11 @@ static InterpretResult run() {
 
 	case OP_NEGATE: push(-pop()); break;
 
+	case OP_ADD: BINARY_OP(+); break;
+	case OP_SUBTRACT: BINARY_OP(-); break;
+	case OP_MULTIPLY: BINARY_OP(*); break;
+	case OP_DIVIDE: BINARY_OP(/); break;
+
 	case OP_RETURN: {
 	    printValue(pop());
 	    printf("\n");
@@ -75,4 +87,5 @@ static InterpretResult run() {
     }
 #undef	READ_BYTE
 #undef	READ_CONSTANT
+#undef	BINARY_OP
 }
