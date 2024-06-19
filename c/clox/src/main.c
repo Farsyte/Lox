e1b385dc4d49b6288947b5e3bc0c2abe6ccfda38
Greg Limes <greg.limes@gmail.com>
2024-06-19 16:36:57 -0400

    start the clox subproject
    - common.h to include common headers
    - main.c with main function
    - GNUmakefile asking for "c99" and building "clox"
    . bin/.gitignore tells Git to ignore the "clox" binary

---
+++ c/clox/src/main.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+int main(int argc, char **argv)
+{
+    (void)argc;         /* intentionally not used */
+    (void)argv;         /* intentionally not used */
+
+    return 0;
+}
