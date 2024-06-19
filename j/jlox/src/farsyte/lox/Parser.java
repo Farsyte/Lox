69e4212544de976984432dd55f405f32357c52f0
Greg Limes <greg.limes@gmail.com>
2024-06-18 20:31:47 -0400

    add Parser.classDeclaration
    This requires fixing the return type of function()
    to be Stmt.Function. Hmmm.

--- j/jlox/src/farsyte/lox/Parser.java
+++ j/jlox/src/farsyte/lox/Parser.java
@@ -29,6 +29,7 @@ class Parser {
     private Stmt declaration() {
 	// declaration → varDecl | statement ;
 	try {
+	    if (match(CLASS)) return classDeclaration();
 	    if (match(FUN)) return function("function");
 	    if (match(VAR)) return varDeclaration();
 	    return statement();
@@ -36,6 +37,20 @@ class Parser {
 	    synchronize();
 	    return null;
 	}
+    }
+
+    private Stmt classDeclaration() {
+	Token name = consume(IDENTIFIER, "Expect class name.");
+	consume(LEFT_BRACE, "Expect '{' before class body.");
+
+	List<Stmt.Function> methods = new ArrayList<>();
+	while (!check(RIGHT_BRACE) && !isAtEnd()) {
+	    methods.add(function("method"));
+	}
+
+	consume(RIGHT_BRACE, "Expect '}' after class body.");
+
+	return new Stmt.Class(name, methods);
     }
 
     private Stmt varDeclaration() {
@@ -50,7 +65,7 @@ class Parser {
 	return new Stmt.Var(name, initializer);
     }
 
-    private Stmt function(String kind) {
+    private Stmt.Function function(String kind) {
 	// funDecl → "fun" function ;
 	// function → IDENTIFIER "(" parameters? ")" block ;
 	// parameters → IDENTIFIER ( "," IDENTIFIER )* ;
